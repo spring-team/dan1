79dea2c12df1336b737115f8febe4e4762ddd10d
Rod Johnson <johnsonroda@gmail.com>
2018-11-20 16:38:11 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/dan/Dan1Application.java
@@ -0,0 +1,12 @@
+package com.dan;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dan1Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Dan1Application.class, args);
+	}
+
+}
